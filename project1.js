63c1d29471c44ef4d09658df3367756ec55ef3bd
arturW15 <126724378+arturW15@users.noreply.github.com>
2023-03-02 23:35:23 +0200

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,32 @@
+const openModal = document.querySelector('.button');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.buttonclose');
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
+modal.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target === modal) modal.close();
+})
+
+var hername;
+function fugive(el) {
+  hername = el.value;
+}
+
+function fuchek() {
+  console.log(hername);
+  if (hername == 'Аліна' || hername == 'аліна' || hername == 'Ангеліна' || hername == 'ангеліна' || hername == 'Аліна Єрмійчук') {
+  window.location = 'https://www.youtube.com/';
+  }
+  else if (hername == 'Денис') {
+     document.write('Ти жартуєш?');
+  }
+  else {
+     document.write('Вибачте, але ваше ім’я не підійшло');
+  }
+}
